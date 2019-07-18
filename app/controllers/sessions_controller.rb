20898400d9d5e52a98d7a29fa03dfb261652fa47
Mohammed Noori <kmanoorib@gmail.com>
2019-07-18 23:26:38 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,17 @@ class SessionsController < ApplicationController
   end
   
   def create
-   # @user = User.find_by(name: params[:name])
     user = User.find_by(name: params[:user][:name])
+
     user = user.try(:authenticate, params[:user][:password])
-    # return head(:forbidden) unless @user.authenticate(params[:password])
-    session[:user_id] = @user.id
+
+    return redirect_to(controller: 'sessions', action: 'new') unless user
+
+    session[:user_id] = user.id
+
+    @user = user
+
+    redirect_to controller: 'welcome', action: 'home'
   end
   
   def destroy
