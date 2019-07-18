bdaff637a32dccfdbe357671f7916dc6e0bdd6be
Mohammed Noori <kmanoorib@gmail.com>
2019-07-18 23:21:31 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(:name, :password)
   end
   
 end
