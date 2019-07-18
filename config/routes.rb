d5325f08d3c791a38ed7eea8dbc7f2f71fdfe906
Mohammed Noori <kmanoorib@gmail.com>
2019-07-18 23:32:25 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get '/home'
+  root 'welcome#home'
+  
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
 end
